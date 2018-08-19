115c0af904ca7dabe175ae2933087e91718fae35
Philip Stanislaus <stanislaus.philip@gmail.com>
2018-08-19 14:59:36 +0200

    Fix broken border in Safari

--- src/components/PaneHeader.tsx
+++ src/components/PaneHeader.tsx
@@ -11,6 +11,7 @@ export const PaneHeader = styled.h2`
   max-width: 400px;
   margin: 0 0 7px 0;
   padding: 0 0 6px 0;
+  border-width: 0;
   border-bottom: 1px solid #18E4CF;
   border-image: linear-gradient(to right, ${colors.lightningBlue}, ${colors.yggdrasilGreen}) 1;
 `
