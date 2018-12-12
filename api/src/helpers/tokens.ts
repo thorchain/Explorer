ea96cf4f6ab7b74f6347a28d6984fcdf1b2e21e2
haneefghanim <haneef.ghanim@gmail.com>
2018-12-11 23:06:41 -0500

    Created APIs to be consumed by trading view chart library.

---
+++ api/src/helpers/tokens.ts
@@ -0,0 +1,54 @@
+/**
+ * List of all tokens and associated useful info
+ */
+export const tokens = {
+  'ADA': {
+    denom: 'ADA',
+    name: 'ADA',
+  },
+  'BTC': {
+    denom: 'BTC',
+    name: 'Bitcoin',
+  },
+  'CAN': {
+    denom: 'CAN',
+    name: 'CanYaCoin',
+  },
+  'DASH': {
+    denom: 'DASH',
+    name: 'Dash',
+  },
+  'EOS': {
+    denom: 'EOS',
+    name: 'EOS',
+  },
+  'ETH': {
+    denom: 'ETH',
+    name: 'Ethereum',
+  },
+  'LOKI': {
+    denom: 'LOKI',
+    name: 'Loki',
+  },
+  'LTC': {
+    denom: 'LTC',
+    name: 'Litecoin',
+  },
+  'NEO': {
+    denom: 'NEO',
+    name: 'NEO',
+  },
+  'XEM': {
+    denom: 'XEM',
+    name: 'New Economy Movement',
+  },
+  'XMR': {
+    denom: 'XMR',
+    name: 'Monero',
+  },
+  'XRP': {
+    denom: 'XRP',
+    name: 'Ripple',
+  },
+}
+
